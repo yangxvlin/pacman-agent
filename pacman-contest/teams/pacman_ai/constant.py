79331eda34d3c30cd7fb4ebaf566419c1a217cca
yangxvlin <1768528843@qq.com>
2020-01-28 16:35:18 +0800

    add constants used in the project

---
+++ pacman-contest/teams/pacman_ai/constant.py
@@ -0,0 +1,12 @@
+"""
+Author:      XuLin Yang
+Student id:  904904
+Date:        2020-1-28 15:38:54
+Description: some constants used
+"""
+
+""" negative infinity used in program """
+NEGATIVE_INFINITY = float('-inf')
+
+""" positive infinity used in program """
+POSITIVE_INFINITY = float('inf')
